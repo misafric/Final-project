9dad60cc4e793f12112cd4e44e09543384d968c3
misafric <fricovam4@gmail.com>
2020-11-23 11:19:10 +0100

    home page styling

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
+<div class="nav-container">
+@foreach($tags as $t)
+        <nav class="nav-{{ $t->name }}">
+            <a href="#  {{ $t->name }} ">{{ $t->name }}</a>
+        </nav>
+@endforeach
+</div>
+
 @foreach($tags as $t)
     <div class="img-background img-{{ $t->id }}">	
         <div class="inside">
